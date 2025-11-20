83741a8d941bd8327cb80decc6b3a6588cb7cd98
VN <vadim.niedens@hsw-stud.de>
2025-11-20 10:35:37 +0100

    Was Das

--- .idea/src/hsw/MethodCollector.java
+++ .idea/src/hsw/MethodCollector.java
@@ -10,6 +10,7 @@ public class MethodCollector {
         // Erste Methode
         System.out.println("Method " + methodCounter + ":" );
         aFirstMethod();
+        VNderBeste();
         methodCounter++;
 // hier kann die nächste Methode folgen.
     }
@@ -18,6 +19,10 @@ public class MethodCollector {
     private static void aFirstMethod() {
         System.out.println("MethodCollector.aFirstMethod: This method has been created by ");
     }
+    private static void VNderBeste() {
+        System.out.println("MethodCollector.aFirstMethod: This method has been created by Vadim ");
+    }
+
 
 
 }
